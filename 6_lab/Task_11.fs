5446a84f20c89fb5b98a7d3b894d9139107c5bf8
Memeladon <bingorussian@mail.ru>
2022-04-12 11:34:16 +0300

    Update Task_11.fs

--- 6_lab/Task_11.fs
+++ 6_lab/Task_11.fs
@@ -6,3 +6,8 @@ let rec createList size =
         let head = Console.ReadLine() |> Int32.Parse
         let tail = createList (size - 1)
         head::tail
+
+[<EntryPoint>]
+let main argv = 
+    printfn "Enter the number of list size: "
+    let list = (createList (Console.ReadLine() |> Int32.Parse)) (fun a b c -> a + b + c))
