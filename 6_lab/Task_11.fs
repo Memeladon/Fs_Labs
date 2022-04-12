88aec193355814b7829aac7b5f0fda311e7ec042
Memeladon <bingorussian@mail.ru>
2022-04-12 11:31:22 +0300

    Update Task_11.fs

--- 6_lab/Task_11.fs
+++ 6_lab/Task_11.fs
@@ -1,1 +1,8 @@
+open System
 
+let rec createList size = 
+    if size <= 0 then []
+    else 
+        let head = Console.ReadLine() |> Int32.Parse
+        let tail = createList (size - 1)
+        head::tail
