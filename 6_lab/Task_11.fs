ea3cab3c5d357af17d9a9ca0f17eb9565f338d88
Memeladon <bingorussian@mail.ru>
2022-04-12 11:38:40 +0300

    Update Task_11.fs

--- 6_lab/Task_11.fs
+++ 6_lab/Task_11.fs
@@ -1,13 +1,22 @@
 open System
 
-let rec createList size = 
+let rec CreateList size = 
     if size <= 0 then []
     else 
         let head = Console.ReadLine() |> Int32.Parse
         let tail = createList (size - 1)
         head::tail
 
+let rec WriteList list = 
+    match list with
+    |head::tail ->
+        printfn "%O" head
+        writeList tail
+    |[] -> ()
+
 [<EntryPoint>]
 let main argv = 
     printfn "Enter the number of list size: "
-    let list = (createList (Console.ReadLine() |> Int32.Parse)) (fun a b c -> a + b + c))
+    let list = (CreateList (Console.ReadLine() |> Int32.Parse)) (fun a b c -> a + b + c))
+    WriteList list
+    0
