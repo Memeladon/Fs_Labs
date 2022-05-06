a9d017ff4a8bcb17b50a9c04c667a4007f6f4848
Memeladon <bingorussian@mail.ru>
2022-05-06 18:27:01 +0300

    Add files via upload

---
+++ 6_lab/Task_12.11.fs
@@ -0,0 +1,30 @@
+﻿open System
+
+let counter list el =
+    let rec subCounter list element count  =
+        match list with
+        | [] -> count
+        | head::tail ->
+            let newCount = if (head = element) then count + 1 else count
+            subCounter tail element newCount
+    subCounter list el 0
+
+let findFunc list =
+    let rec subFunc list primalList element =
+        match list with
+        | [] -> element
+        | head::tail ->
+            let newElement = if (counter primalList head) = 1 then head else element
+            subFunc tail primalList newElement
+    subFunc list list list.Head
+
+[<EntryPoint>]
+let main argv =
+
+    printfn "Количество элементов и список:"
+
+    let list = [7;8;7;7;7;7;7;7]
+   
+    printfn "Результат: %d" (findFunc list)
+
+    0
